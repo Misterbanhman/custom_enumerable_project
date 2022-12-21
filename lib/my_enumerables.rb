4bb40fb5b45a8c4318af2af9e3dcd4cc3dd6c9f3
misterbanhman <nathan.i.banh@gmail.com>
2022-12-20 23:17:24 -0700

    Work on completing exercises.

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,18 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each()
+    self.each { |value| yield value } 
+  end
+
+  def my_each_with_index()
+    if block_given?
+      for i in 0..self.size-1 do
+        yield self[i], i
+      end
+    else
+      self
+    end
+  end
+
 end
